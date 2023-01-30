0813c543675917e90b6305a22eed1bac14702fd4
zine-fj <747810974@qq.com>
2023-01-30 11:39:16 +0800

    🚧 eslint可以用了

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,13 @@ module.exports = {
   },
   extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    "prettier",
   ],
   parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
     parser: "@babel/eslint-parser",
   },
+  plugins: ["prettier", "@babel", "simple-import-sort"],
   rules: {},
 };
